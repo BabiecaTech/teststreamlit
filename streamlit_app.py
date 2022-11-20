e0988911849352cb64634a6275644a758e5c2107
BabiecaTech <39100687+BabiecaTech@users.noreply.github.com>
2022-11-20 15:58:42 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,2 @@
 #pip install streamlit
-#streamlit hello
-import streamlit as st 
-import pandas as pd 
-import base64 
-from sklearn.linear_model import LogisticRegression 
-from sklearn.model_selection import train_test_split 
-from sklearn.preprocessing import MinMaxScaler 
-import numpy as np 
-import pickle as pkl 
-import shap 
-import streamlit.components.v1 as components
+streamlit hello
